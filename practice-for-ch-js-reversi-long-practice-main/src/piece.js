42cb0f19ea92fc33e3d8fe00ed3c1d7179f2bb9e
Jordy <jordycorporan95@gmail.com>
2023-06-23 14:19:58 -0400

    Fixed bug

--- practice-for-ch-js-reversi-long-practice-main/src/piece.js
+++ practice-for-ch-js-reversi-long-practice-main/src/piece.js
@@ -8,27 +8,27 @@ function Piece (color) {
 /**
  * Returns the color opposite the current piece.
  */
-Piece.prototype.oppColor = function() {
-  if (this.color === 'white') {
-    return 'black';
-  } else {
-    this.color = 'white';
-    return this.color;
-  }
+Piece.prototype.oppColor = function () {
 };
-
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+  if (this.color === 'white') {
+    this.color = 'black';
+  } else {
+    this.color = 'white';
+  }
 };
+
 
 /**
  * Returns a string representation of the string
  * based on its color.
  */
 Piece.prototype.toString = function () {
+  
 };
 
 // DON'T TOUCH THIS CODE
